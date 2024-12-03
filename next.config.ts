fea095b0a583c7616769436a98552dc1a36cbbde
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-12-03 19:47:52 +0530

    initialize tiptap editor

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Webpack: Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs');
+    }
+    return config;
+  },
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        // Turbopack: Ensure that all imports of 'yjs' resolve to the same instance
+        yjs: path.resolve(__dirname, 'node_modules/yjs'),
+      },
+    },
+  },
 };
 
 export default nextConfig;
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from './editor';
+
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
@@ -5,6 +7,10 @@ interface DocumentIdPageProps {
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
 
-  return <div>DocumentId: {documentId}</div>;
+  return (
+    <div className="min-h-screen bg-[#fafbfd]">
+      <Editor />
+    </div>
+  );
 };
 export default DocumentIdPage;
