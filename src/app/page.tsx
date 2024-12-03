4c1d69ba1f809f367808185702cf69807abcb1ac
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-12-03 18:44:20 +0530

    removed unwanted assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold text-emerald-500">Hello, World!</h1>
+      <Button variant="destructive" size="lg">
+        Click me
+      </Button>
     </div>
   );
 };
