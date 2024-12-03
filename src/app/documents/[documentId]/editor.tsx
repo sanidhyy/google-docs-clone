366c4f5f68b9e1dd650319d0474be394f00ed3ba
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-12-03 21:00:18 +0530

    tiptap required extensions and styles added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import Image from '@tiptap/extension-image';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import ImageResize from 'tiptap-extension-resize-image';
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,8 +20,24 @@ export const Editor = () => {
           'focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14 cursor-text',
       },
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+      Image,
+      ImageResize,
+      StarterKit,
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+    ],
+    content: `
+      <h1>Hello World!</h1>
+    `,
     immediatelyRender: false,
   });
 
