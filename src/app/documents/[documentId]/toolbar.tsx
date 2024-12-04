c3ff410114445c6d0e6f9b5b94aec52835bb5ef5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-12-04 19:27:56 +0530

    font size implemented

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -20,6 +20,62 @@ import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 import { useEditorStore } from '@/store/use-editor-store';
 
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    { label: 'Heading 1', value: 1, fontSize: '32px' },
+    { label: 'Heading 2', value: 2, fontSize: '24px' },
+    { label: 'Heading 3', value: 3, fontSize: '20px' },
+    { label: 'Heading 4', value: 4, fontSize: '18px' },
+    { label: 'Heading 5', value: 5, fontSize: '16px' },
+    { label: 'Heading 6', value: 6, fontSize: '16px' },
+    { label: 'Normal text', value: 0, fontSize: '16px' },
+  ] as const;
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= headings.length; level++) {
+      if (editor?.isActive('heading', { level })) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return headings.at(-1)?.label;
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurrentHeading()}</span>
+
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <DropdownMenuItem key={value} asChild>
+            <button
+              onClick={() => {
+                if (value === 0) editor?.commands.setParagraph();
+                else editor?.commands.toggleHeading({ level: value });
+              }}
+              className={cn(
+                'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+                (value === 0 && !editor?.isActive('heading')) || (editor?.isActive('heading', { level: value }) && 'bg-neutral-200/80'),
+              )}
+              style={{ fontSize }}
+            >
+              {label}
+            </button>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
 
@@ -172,7 +228,7 @@ export const Toolbar = () => {
 
       <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO: Heading */}
+      <HeadingLevelButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Font Size */}
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
