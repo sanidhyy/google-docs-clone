7d2a4130df1a0382b456834fea68800a015ec4c8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-01-17 22:27:34 +0530

    feat: navbar ui

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from './editor';
+import { Navbar } from './navbar';
 import { Toolbar } from './toolbar';
 
 interface DocumentIdPageProps {
@@ -10,8 +11,14 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="min-h-screen bg-[#fafbfd]">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 inset-x-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
