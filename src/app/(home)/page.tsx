c078c2efea0ffb64eb022ad3b2c04b22987270c3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-01-18 20:38:46 +0530

    feat: convex schema created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useQuery } from 'convex/react';
+
+import { api } from '@/../convex/_generated/api';
+
 import { Navbar } from './navbar';
 import { TemplateGallery } from './template-gallery';
 
 const HomePage = () => {
+  const documents = useQuery(api.documents.get);
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 inset-x-0 z-10 h-16 bg-white p-4">
@@ -10,6 +18,7 @@ const HomePage = () => {
 
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => <span key={document._id}>{document.title}</span>)}
       </div>
     </div>
   );
