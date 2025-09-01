32c9f66096009bbc1e0c1654ee6017c74dd1bf23
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-09-01 20:25:12 +0530

    build(deps): update @liveblocks/node and Tiptap extensions
    Bumps the following dependencies:
    - @liveblocks/node from 3.3.0 to 3.4.0
    - @tiptap/extension-color from 2.24.3 to 3.3.0
    - @tiptap/extension-table and related extensions (cell, header, row, task-item, task-list) from 2.24.3 to 3.3.0
    - @tiptap/extension-text-style from 2.24.3 to 3.3.0
    - eslint-config-next from 15.4.6 to 15.5.0
    
    Updated imports in editor.tsx for better structure.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,19 +2,15 @@
 
 import { useLiveblocksExtension } from '@liveblocks/react-tiptap';
 import { useStorage } from '@liveblocks/react/suspense';
-import { Color } from '@tiptap/extension-color';
 import FontFamily from '@tiptap/extension-font-family';
 import Highlight from '@tiptap/extension-highlight';
 import Image from '@tiptap/extension-image';
 import Link from '@tiptap/extension-link';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
+import { Table, TableCell, TableHeader, TableRow } from '@tiptap/extension-table';
 import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
 import TextAlign from '@tiptap/extension-text-align';
-import TextStyle from '@tiptap/extension-text-style';
+import { Color, TextStyle } from '@tiptap/extension-text-style';
 import Underline from '@tiptap/extension-underline';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
